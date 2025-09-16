0015dfebf68ee5eac391d97255814aae0f002b02
xkazu0x <kazuooficial@hotmail.com>
2025-09-16 00:14:01 -0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for arg in "$@"; do declare $arg='1'; done
+
+mkdir -p build
+
+javac -sourcepath ../src/ -cp ../lib/* -d ../build/ src/*.java
+
+if [ -v run ]; then
+    cd build
+    java Main
+    cd ..
+fi
