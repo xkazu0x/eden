adedc8c136ac096a8d5649f1a2c7cc6b2843d2f3
xkazu0x <kazuooficial@hotmail.com>
2025-09-18 01:01:23 -0300

    on my mama this is not getting touched anymore

--- build.bat
+++ build.bat
@@ -2,20 +2,30 @@
 setlocal enabledelayedexpansion
 cd /D "%~dp0"
 
-set CLASSPATH=.;lib\antlr-4.13.2-complete.jar;%CLASSPATH%
+set lib_dir=%CD%\lib
+set antlr_lib=%lib_dir%\antlr-4.13.2-complete.jar
+
+set CLASSPATH=%lib_dir%;%antlr_lib%;%CLASSPATH%
 
 for %%a in (%*) do set "%%a=1"
 
-set antlr4=java -Xmx500M -cp lib\antlr-4.13.2-complete.jar;%CLASSPATH% org.antlr.v4.Tool
-set grun=java -Xmx500M -cp lib\antlr-4.13.2-complete.jar;%CLASSPATH% org.antlr.v4.gui.TestRig
+set antlr4=java -cp %antlr_lib%;%CLASSPATH% org.antlr.v4.Tool
+set grun=java -cp %antlr_lib%;%CLASSPATH% org.antlr.v4.gui.TestRig
 
-set compile=javac -sourcepath src -d build -cp lib\antlr-4.13.2-complete.jar
+set compile=javac -sourcepath src -d build
 set execute=java
 
 if not exist build mkdir build
 
 %antlr4% -o src -visitor Eden.g4
 %compile% src\*.java
+
+if "%test%"=="1" (
+    copy Eden.g4 build/Eden.g4
+    cd build
+    %grun% Eden prog -gui ..\example.eden
+    cd ..
+)
 
 if "%run%"=="1" (
     cd build
